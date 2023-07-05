f592802cd6140cba8aeb8940cf4d4d9486058b87
Pongpisud1998 <83650472+Pongpisud1998@users.noreply.github.com>
2023-07-05 16:33:32 +0700

    map

--- hummingmap/src/app/app-routing.module.ts
+++ hummingmap/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+HomeComponent
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: false }),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
